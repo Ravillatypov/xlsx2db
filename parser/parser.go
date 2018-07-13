c6a954ee813176d9077c5a5b8281f545e73d155f
Ravil Latypov <ravillatypov12@gmail.com>
2018-07-13 20:50:03 +0300

    add Parser.ParseRow & Parser.ParseDevices

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,9 @@ package parser
 
 import (
 	"regexp"
+	"strings"
+
+	"github.com/tealeg/xlsx"
 )
 
 // Device модель
@@ -30,4 +33,34 @@ type Order struct {
 type Parser struct {
 	Devices []Device
 	Reg     regexp.Regexp
+}
+
+// ParseRow получает строку xlsx, парсит и отправляет заявку через канал
+func (p *Parser) ParseRow(r *xlsx.Row, ch chan *Order) int64 {
+	extID, err := r.Cells[1].Int64()
+	if err != nil {
+		return extID
+	}
+	mku, _ := r.Cells[2].Int()
+	mku = mku / 1000
+	address := r.Cells[3].Value
+	phones := p.Reg.FindAllString(r.Cells[4].Value, -1)
+	name := r.Cells[5].Value
+	comment := r.Cells[5].Value
+	devids := p.ParseDevices(&comment)
+	cl := Contact{Name: name, Address: address, Phones: phones}
+	ch <- &Order{ExtID: extID, Mku: int8(mku), Coment: comment, Devices: devids, Client: cl}
+	return extID
+}
+
+// ParseDevices поиск и замена известный устройств
+func (p *Parser) ParseDevices(s *string) []int8 {
+	ids := make([]int8, 0)
+	for _, d := range p.Devices {
+		if strings.Contains(*s, d.Model) {
+			ids = append(ids, d.ID)
+			*s = strings.Replace(*s, d.Model, " ", -1)
+		}
+	}
+	return ids
 }
